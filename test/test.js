7222eb8f2ddee767c9c7fb662c4b54337ae1d624
Pau Fracés <pfraces@gmail.com>
2013-01-18 19:05:38 +0100

    first test!

--- test/test.js
+++ test/test.js
@@ -1,22 +1,55 @@
-var should = require('should');
-
 // # test-js
-//
+
 // Conjunto de tests sobre el comportamiento de JavaScript en diferentes
 // entornos
 //
 // Empezaré testeando el libro 'JavaScript: The Good Parts' bajo `node.js` e
 // iré ampliando progresivamente con diferentes libros y entornos
 
+var should = require('should');
 
 // # "JavaScript: The Good Parts" by Douglas Crockford
 
 describe('JavaScript: The Good Parts', function () {
-  describe('Array', function () {
-    describe('#indexOf()', function () {
-      it('should return -1 when the value is not present', function () {
-        [1,2,3].indexOf(5).should.equal(-1);
-        [1,2,3].indexOf(0).should.equal(-1);
+
+// ## 1. Good Parts
+//
+// >   JavaScript's functions are first class objects with (mostly) lexical
+//     scoping. JavaScript is the first lambda language to go mainstream. Deep
+//     down, JavaScript has more in common with List and Scheme than with Java.
+//     **It's Lisp in C's clothing.** This makes JavaScript a remarkably
+//     powerful language.
+
+// ## 2. Grammar
+
+  describe('2. Grammar', function () {
+
+// ### Reserved words
+
+    describe('Reserved words', function () {
+
+// `abstract`, `boolean`, `break`, `byte`, `case`, `catch`, `char`, `class`,
+// `const`, `continue`, `debugger`, `default`, `delete`, `do`, `double`,
+// `else`, `enum`, `export`, `extends`, `false`, `final`, `finally`, `float`,
+// `for`, `function`, `goto`, `if`, `implements`, `import`, `in`, `instanceof`,
+// `int`, `interface`, `long`, `native`, `new`, `null`, `package`, `private`,
+// `protected`, `public`, `return`, `short`, `static`, `super`, `switch`,
+// `synchronized`, `this`, `throw`, `throws`, `transient`, `true`, `try`,
+// `typeof`, `var`, `volatile`, `void`, `while`, `with`
+//
+// >   Most of the reserved words in this list are not used in the language.
+//     The list does not include some words that should have been reserved but
+//     were not, such as `undefined`, `NaN`, and `Infinity`.
+
+// >   It is not permitted to name a variable or parameter with a reserved
+//     word. Worse, it is not permitted to use a reserved word as the name of
+//     an object property in an object literal or following a dot in a
+//     refinement.
+
+      it('should throw if "in" is used as a variable', function () {
+        (function () {
+          eval("var in;");
+        }).should.throw();
       });
     });
   });
