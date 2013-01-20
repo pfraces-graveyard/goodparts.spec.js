5c40424c36cc1454f19002c0185642e7925abc23
Pau Fracés <pfraces@gmail.com>
2013-01-20 02:08:32 +0100

    [min] grammar fix

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 // # test-js
 
-// Testing JavaScript internals
+// Testing JavaScript internals.
 //
 // I will start testing which is said in 'JavaScript: The Good Parts' under
 // `node.js`. Future updates will include more readings and more environments.
